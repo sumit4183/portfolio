d6978531aad2ca1515179924525920c47412e1ca
SUMIT <sumit4183@yahoo.com>
2024-06-18 13:48:39 -0700

    Initial Setup

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Experience from "@/components/Experience";
+import Tech from "@/components/Tech";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <main className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Projects />
+      <div className="relative z-0"> 
+        <Contact />
+      </div>
+    </main>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
