e0635fffd65d2446c7be9f5a107a21e529333050
SUMIT <sumit4183@yahoo.com>
2024-07-03 11:13:19 -0700

    Projects Section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,118 @@
+"use client"
+
 import React from 'react'
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+
+import SectionWrapper from "./SectionWrapper.jsx"
+import { projects } from "@/constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  website_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)} className="flex">
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl w-full  flex flex-col justify-between'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          {source_code_link && (
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src="/github.png"
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          )}
+          </div>
+        </div>
+
+        <div className='mt-5 flex-grow'>
+          <div>
+            <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          </div> 
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2 items-end'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+          <div className="flex-grow"></div>
+          {website_link && (
+            <div
+              onClick={() => window.open(website_link, "_blank")}
+              className='rounded-full items-center cursor-pointer flex'
+            >
+              <img
+                src="/externlink.svg"
+                alt="External Link Symbol"
+                className="w-4 h-4 pb-2/3"
+              />
+              <span className="pl-1">Site</span>
+            </div>
+          )}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className="sectionHeadText">Projects</h2>
+      </motion.div>
 
-export default Projects
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <div className='mt-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Projects, "projects");
